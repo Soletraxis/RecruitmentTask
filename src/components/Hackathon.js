0f3a4b43904389c12b57bbc93a48084e009e8b7d
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-10-05 19:34:07 +0200

    Add carousel to Hackathon page

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 import "./Hackathon.css"
-
+import { Carousel } from 'react-bootstrap'
+import PrzemyslawImg from '../img/ct/przemyslaw.jpg'
+import DominikImg from '../img/ct/dominik.jpg'
 
 class Hackathon extends Component {
     render() {
+      const carouselContent = [ DominikImg, PrzemyslawImg, PrzemyslawImg]
+
         return (
             <div className="hackathon">
-                Hackathon
+                <Carousel
+                  bsClass='carousel'
+                >
+                  {carouselContent.map((user) =>
+                    <Carousel.Item>
+                      <div className='containerImg'>
+                        <img  alt='900x600' src={user} />
+                      </div>
+                    </Carousel.Item>
+                  )}
+                </Carousel>
             </div>
         );
     }
