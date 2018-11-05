45589b5a7aa5cb96a700d41263add9d4e5a2a1c4
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-11-05 23:24:49 +0100

    Create request to post to database

--- src/components/Register.js
+++ src/components/Register.js
@@ -31,37 +31,47 @@ class Register extends Component {
     }
   }
 
-  formularField = (data) => {
-    return(
-      <div>
-        <h1>{data.question}</h1>
-        <input placeholder={data.placeholder}/>
-      </div>
-    )
+  validate = (req) => {
+    const chuj = Object.keys(req);
+
+    const xd = chuj.filter((q) => {
+      return req[q].length === 0;
+    })
+
+    return xd.length;
   }
 
-  dottedField = (data) => {
-    return (
-      <form>
-        <label className="checkbox-inline"><input type="checkbox" value=""/>Option 1</label>
-      </form>
-    )
+  submitForm = () => {
+    const req = {teamName: document.getElementById('teamName').value.trim()}
+
+    this.state.participantNumber.map((i) => {
+      const email = document.getElementById(`email${i}`).value.trim();
+      const name = document.getElementById(`name${i}`).value.trim();
+      Object.assign(req, {[`email${i}`]: email, [`name${i}`]: name});
+    })
+    if(this.validate(req) !== 0) {
+      // odrzuć
+      console.log('ODRZUCAM')
+    } else {
+      console.log('PRZYJME')
+      //firebase request
+    }
   }
 
     render() {
         return (
             <div className="register">
                 <Grid>
-                  <form action={f=>f}>
+                  <form>
                     <label className="checkbox-inline" onClick={() => this.changeCompetitionType(true)}><input type="checkbox" checked={this.state.isClassic}/>Część Klasyczna</label>
                     <label className="checkbox-inline" onClick={() => this.changeCompetitionType(false)}><input type="checkbox" checked={!this.state.isClassic}/>Część Robotyczna</label>
                     <div>
-                      <div className='form-group'><label>Nazwa drużyny:  </label><input type='text'/></div>
+                      <div className='form-group'><label>Nazwa drużyny:  </label><input type='text' id={'teamName'}/></div>
                       {this.state.participantNumber.map((i) => {
                         return(
                           <div className="form-group" key={i}>
-                            <label>Email:  </label><input type='text'/>
-                            <label>Imię i nazwisko: <input type='text'/></label>
+                            <label>Email:  </label><input type='text' id={`email${i}`}/>
+                            <label>Imię i nazwisko: <input type='text' id={`name${i}`}/></label>
                             {i === this.state.participantNumber.length && i !== 1 &&
                             <span className="glyphicon glyphicon-minus" onClick={this.decreaseParticipant}/> }
                           </div>
@@ -69,13 +79,9 @@ class Register extends Component {
                       }) }
                     </div>
                     {this.state.participantNumber.length < 4 && <span className="glyphicon glyphicon-plus-sign" onClick={this.addParticipant}/>}
-                  {/*{FormularData.map((formField) => {
-                    formField.isForm ? this.formularField(formField) : this.dottedField(formField)
-                    this.dottedField(2);
-                  })}*/}
                   </form>
 
-                  <button type='submit' color='orange' className='button' onClick={f=>f}>Zgłaszam sprzeciw!</button>
+                  <button type='submit' color='orange' className='button' onClick={this.submitForm}>Zgłaszam sprzeciw!</button>
                     {/*<h1>A może by tak stworzyć system rejestracyjny?</h1>
                     <Image src={require('../img/elon.jpg')}/>*/}
                 </Grid>
