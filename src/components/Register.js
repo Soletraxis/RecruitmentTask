608ec1d837b2836035f4f8b8aaedd1891da720de
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-11-05 10:56:16 +0100

    WIP: dodane checkboxy konkurencji i wstępnie pola

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,6 +6,14 @@ import FormularData from '../Constants/FormularData'
 
 
 class Register extends Component {
+  state = {
+    isClassic: true
+  }
+
+  changeCompetitionType = (type) => {
+    this.setState({ isClassic: type })
+  }
+
   formularField = (data) => {
     return(
       <div>
@@ -17,9 +25,9 @@ class Register extends Component {
 
   dottedField = (data) => {
     return (
-      <Row>
-
-      </Row>
+      <form>
+        <label className="checkbox-inline"><input type="checkbox" value=""/>Option 1</label>
+      </form>
     )
   }
 
@@ -27,9 +35,20 @@ class Register extends Component {
         return (
             <div className="register">
                 <Grid>
-                  {FormularData.map((formField) => {
+                  <form>
+                    <label className="checkbox-inline" onClick={() => this.changeCompetitionType(true)}><input type="checkbox" checked={this.state.isClassic}/>Część Klasyczna</label>
+                    <label className="checkbox-inline" onClick={() => this.changeCompetitionType(false)}><input type="checkbox" checked={!this.state.isClassic}/>Część Robotyczna</label>
+                    <div>
+                      {[1,2,3,4].map(() => {
+                        return(<div><input type='text'/></div>)
+                      }) }
+                    </div>
+
+                  {/*{FormularData.map((formField) => {
                     formField.isForm ? this.formularField(formField) : this.dottedField(formField)
-                  })}
+                    this.dottedField(2);
+                  })}*/}
+                  </form>
 
                     {/*<h1>A może by tak stworzyć system rejestracyjny?</h1>
                     <Image src={require('../img/elon.jpg')}/>*/}
