9bf70610587e522452ed5d2bade30aa99be45659
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-11-07 13:44:08 +0100

    Add onClick property on FAQ header

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -96,14 +96,14 @@ class Hackathon extends Component {
                             onSelect={this.handleSelect}
                         >
                             <Panel eventKey="1">
-                                <Panel.Heading>
-                                    <Panel.Title toggle>Panel heading 1</Panel.Title>
+                                <Panel.Heading onClick={() => this.handleSelect('1')}>
+                                    <Panel.Title>Panel heading 1</Panel.Title>
                                 </Panel.Heading>
                                 <Panel.Body collapsible>Panel content 1</Panel.Body>
                             </Panel>
                             <Panel eventKey="2">
-                                <Panel.Heading>
-                                    <Panel.Title toggle>Panel heading 2</Panel.Title>
+                                <Panel.Heading onClick={() => this.handleSelect('2')}>
+                                    <Panel.Title>Panel heading 2</Panel.Title>
                                 </Panel.Heading>
                                 <Panel.Body collapsible>Panel content 2</Panel.Body>
                             </Panel>
