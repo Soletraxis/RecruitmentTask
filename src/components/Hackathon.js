60ce74fa99adfd60865786eed3b3f4afcb1b23b7
Przemysław Przebieglec <p.przebieglec@gmail.com>
2018-11-07 13:29:55 +0100

    Change FAQ place

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -86,28 +86,29 @@ class Hackathon extends Component {
                         )
                     </Carousel>
                 </div>
-
-                <div className="faq">
-                    <h3>Frequently Asked Questions</h3>
-                    <PanelGroup
-                        accordion
-                        id="accordion-controlled-example"
-                        activeKey={this.state.activeKey}
-                        onSelect={this.handleSelect}
-                    >
-                        <Panel eventKey="1">
-                            <Panel.Heading>
-                                <Panel.Title toggle>Panel heading 1</Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body collapsible>Panel content 1</Panel.Body>
-                        </Panel>
-                        <Panel eventKey="2">
-                            <Panel.Heading>
-                                <Panel.Title toggle>Panel heading 2</Panel.Title>
-                            </Panel.Heading>
-                            <Panel.Body collapsible>Panel content 2</Panel.Body>
-                        </Panel>
-                    </PanelGroup>
+                <div className='faq-container'>
+                    <div className="faq">
+                        <h3>Frequently Asked Questions</h3>
+                        <PanelGroup
+                            accordion
+                            id="accordion-controlled-example"
+                            activeKey={this.state.activeKey}
+                            onSelect={this.handleSelect}
+                        >
+                            <Panel eventKey="1">
+                                <Panel.Heading>
+                                    <Panel.Title toggle>Panel heading 1</Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 1</Panel.Body>
+                            </Panel>
+                            <Panel eventKey="2">
+                                <Panel.Heading>
+                                    <Panel.Title toggle>Panel heading 2</Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>Panel content 2</Panel.Body>
+                            </Panel>
+                        </PanelGroup>
+                    </div>
                 </div>
             </div>
         );
