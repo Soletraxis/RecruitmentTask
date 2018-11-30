a489d22cc7ac760f1dc2e611c99c4632c56958a1
kolorowerowe <34072563+kolorowerowe@users.noreply.github.com>
2018-11-30 23:57:27 +0100

    added all sponsors

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -118,8 +118,73 @@ class Partners extends Component {
 
                             </p>
 
-                            <i className="fa fa-globe" aria-hidden="true"></i> <a href="http://nokiakrakow.pl/" className="partnerLink">nokiakrakow.pl/</a><br/>
+                            <i className="fa fa-globe" aria-hidden="true"></i> <a href="http://nokiakrakow.pl/" className="partnerLink">nokiakrakow.pl</a><br/>
                             <i className="fab fa-facebook-square fa-lg"></i> <a href="https://www.facebook.com/rdkrakow/" className="partnerLink">/rdkrakow/</a><br/>
+
+                        </Col>
+
+                    </Row>
+
+                    <Row className="Sponsor">
+                        <Col xs={12} sm={4} className="Simg">
+                            <div className="Er_img">
+                                <a href="https://www.ericsson.com/pl/pl"><Image src={require('../img/partners/ericsson.PNG')} /></a>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={8} className="Sdescription">
+                            <h3>Ericsson</h3>
+                            <p>
+                                Ericsson to firma o zasięgu globalnym, założona w 1876 roku w Sztokholmie przez Larsa Magnusa Ericssona. Ericsson jest dostawcą usług i serwisów dla operatorów telekomunikacyjnych. Firma działa w 175 krajach. W Polsce pierwsza centrala telefoniczna Ericsson została uruchomiona w Warszawie w 1904 roku. W skład produktów firmy wchodzą: infrastruktura sieci stałych i mobilnych, internet szerokopasmowy oraz rozwiązania multimedialne. Ericsson jest aktywnym promotorem technologii 5G w Polsce oraz członkiem Porozumienia na rzecz Strategii „5G dla Polski”.
+                            </p>
+                            <p>
+                                Ericsson posiada w Polsce dwa centra badawczo-rozwojowe w Łodzi i Krakowie, zatrudnia blisko 2000 inżynierów odpowiedzialnych za rozwój kolejnych produktów teleinformatycznych Ericsson wykorzystywanych na całym świecie
+                            </p>
+                            <p>
+                                We współczesnym świecie, technologia napędza biznes i zmiany w społeczeństwie. Pracownicy Ericsson to inicjatorzy zmian, łączący inwencję, nieszablonowe myślenie i wiedzę o nowoczesnych technologiach, by kształtować swoje otoczenie. To oni upowszechniają technologię łączności w przemyśle, społeczeństwie i lokalnych społecznościach.                            </p>
+                            <p>
+                                Naszą wizją jest Networked Society – zbudowanie społeczeństwa operującego w sieci, w którym każdy z nas dzięki technologii ma możliwość w pełni wykorzystać swój potencjał. By wprowadzić tę wizję w życie, potrzebujemy odpowiednich ludzi – takich jak Ty.
+                            </p>
+                            <i className="fa fa-globe" aria-hidden="true"></i> <a href="http://nokiakrakow.pl/" className="partnerLink">nokiakrakow.pl</a><br/>
+
+                        </Col>
+
+                    </Row>
+                    <Row className="Sponsor">
+                        <Col xs={12} sm={4} className="Simg">
+                            <div className="Ant_img">
+                                <a href="https://antsolutions.eu/"><Image src={require('../img/partners/ant.png')} /></a>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={8} className="Sdescription">
+                            <h3>Ant</h3>
+                            <p>
+                                Jesteśmy doświadczonymi inżynierami, firmą technologiczną, dostarczającą dedykowane systemy bazując na własnej platformie programistycznej dla największych zakładów produkcyjnych na świecie, ponad 350 projektów w 30 krajach. Tworzymy zaawansowane rozwiązania z dziedziny automatyki i systemów komputerowych, zgodnie z ideą Industry 4.0.
+                              </p>
+                            <p>
+                                W ANT podchodzimy kompleksowo do realizacji projektów. Wspólnie z Klientem opracowujemy koncepcję i strategię wdrożenia, dostarczamy automatykę przemysłową oraz sprzęt, tworzymy i wdrażamy dedykowane oprogramowanie oraz zapewniamy 24/7 support. Wdrażane systemy tworzone są w oparciu o autorską platformę zrealizowaną od podstaw i stale rozwijaną przez nasz zespół.
+                            </p>
+                            <p>
+                                Jesteśmy młodym, aktywnym i szybko rozwijającym się zespołem inżynierów realizującym dedykowane systemy dla międzynarodowych firm produkcyjnych. Jeśli lubisz wyzwania i chcesz wspólnie z nami tworzyć i rozwijać autorskie oprogramowanie implementowane na całym świecie – dołącz do naszego zespołu.                            </p>
+
+                            <i className="fa fa-globe" aria-hidden="true"></i> <a href="https://www.ericsson.com/pl/pl" className="partnerLink">www.ericsson.com/pl/pl</a><br/>
+                        </Col>
+
+                    </Row>
+
+
+                    <Row className="Sponsor">
+                        <Col xs={12} sm={4} className="Simg">
+                            <div className="Ele_img">
+                                <a href="http://www.elettric80.com"><Image src={require('../img/partners/elettric80.jpg')} /></a>
+                            </div>
+                        </Col>
+                        <Col xs={12} sm={8} className="Sdescription">
+                            <h3>Elettric 80</h3>
+                            <p>
+                                Elettric80 jest światowym liderem w dziedzinie inteligentnych w pełni automatycznych systemów logistycznych. Główna siedziba firmy znajduje się we Włoszech. W Polsce ulokowany jest prężnie rozwijający się oddział inżynieryjny z siedzibą w Krakowie. Firma ma swoje oddziały również w USA, Wielkiej Brytanii, Brazylii, Szwecji, Francji, Australii oraz Dubaju. Elettric80 znany jest na świecie głownie z bezobsługowych wózków widłowych sterowanych laserowo LGV, wysp paletyzujących oraz inteligentnych systemów zarządzania magazynem.
+                            </p>
+
+                            <i className="fa fa-globe" aria-hidden="true"></i> <a href="http://www.elettric80.com" className="partnerLink"> www.elettric80.com</a><br/>
 
                         </Col>
 
